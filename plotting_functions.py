5e253ce4d69bf3d709a60bbf285f8277105f539c
matthewcassi <36492182+matthewcassi@users.noreply.github.com>
2018-07-23 22:25:54 -0400

    Add files via upload

--- plotting_functions.py
+++ plotting_functions.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def object_bar_plots(df):
-	'''
-	Function takes a data frame
-	Dataframe is subsetted by taking only the columns with a dtype of 'object'
-	Each column is then used to create a bar plot of the value counts
-	'''
+    '''
+    Function takes a data frame
+    Dataframe is subsetted by taking only the columns with a dtype of 'object'
+    Each column is then used to create a bar plot of the value counts
+    '''
     objects = df.select_dtypes(include='object')
     cols = objects.columns
     for i in cols:
@@ -15,12 +17,12 @@ def object_bar_plots(df):
         plt.ylabel('Count')
         plt.show()
         print('-----------------------------------------------')
-		
+
 def histogram(df, column, log=False):
-	'''
-	Function takes a data frame, a column, and whether you want the log transform
-	The function creates a histogram for the specified column
-	'''
+    '''
+    Function takes a data frame, a column, and whether you want the log transform
+    The function creates a histogram for the specified column
+    '''
     if log == True:
         sns.distplot(df[column].dropna().apply(np.log))
         plt.title('Histogram and Distribution of ' + column)
@@ -29,12 +31,12 @@ def histogram(df, column, log=False):
         sns.distplot(df[column].dropna())
         plt.title('Histogram and Distribution of ' + column)
     plt.show()
-	
+
 def boxplot(df, column, log=False):
-	'''
-	Function takes a data frame, a column, and whether you want the log transform
-	The function creates a boxplot for the specified column
-	'''
+    '''
+    Function takes a data frame, a column, and whether you want the log transform
+    The function creates a boxplot for the specified column
+    '''
     if log == True:
         sns.boxplot(df[column].dropna().apply(np.log), orient='v')
         plt.title('Box Plot of Log ' + column)
@@ -42,4 +44,30 @@ def boxplot(df, column, log=False):
     else:
         sns.boxplot(df[column].dropna(), orient='v')
         plt.title('Box Plot of ' + column)
+    plt.show()
+
+def scatterplot(df, x_var, y_var, log=False):
+    if sum(df[y_var].notnull()) == sum(df[x_var].notnull()):
+        if log == True:
+            plt.scatter(x=df[x_var].dropna(), y=df[y_var].dropna().apply(np.log))
+            plt.title('Scatter Plot of ' + x_var + ' and Log Tranform of ' + y_var)
+            plt.xlabel(x_var)
+            plt.ylabel('Log Transform of ' + y_var)
+        else:
+            plt.scatter(x=df[x_var].dropna(), y=df[y_var].dropna())
+            plt.title('Scatter Plot of ' + x_var + ' and ' + y_var)
+            plt.xlabel(x_var)
+            plt.ylabel(y_var)
+    else:
+        drops = df[[x_var,y_var]].dropna()
+        if log == True:
+            plt.scatter(x=drops[x_var].dropna(), y=drops[y_var].dropna().apply(np.log))
+            plt.title('Scatter Plot of ' + x_var + ' and Log Tranform of ' + y_var)
+            plt.xlabel(x_var)
+            plt.ylabel('Log Transform of ' + y_var)
+        else:
+            plt.scatter(x=drops[x_var].dropna(), y=drops[y_var].dropna())
+            plt.title('Scatter Plot of ' + x_var + ' and ' + y_var)
+            plt.xlabel(x_var)
+            plt.ylabel(y_var)
     plt.show()
