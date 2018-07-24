267e48fa25337949d0374e41f4c4d13fda9c109d
matthewcassi <36492182+matthewcassi@users.noreply.github.com>
2018-07-24 14:50:51 -0400

    Add files via upload

--- plotting_functions.py
+++ plotting_functions.py
@@ -47,27 +47,75 @@ def boxplot(df, column, log=False):
     plt.show()
 
 def scatterplot(df, x_var, y_var, log=False):
+    plt.figure(figsize=(15,9))
     if sum(df[y_var].notnull()) == sum(df[x_var].notnull()):
         if log == True:
-            plt.scatter(x=df[x_var].dropna(), y=df[y_var].dropna().apply(np.log))
+            plt.scatter(x=df[x_var].dropna(), y=df[y_var].dropna().apply(np.log), alpha=0.3) 
             plt.title('Scatter Plot of ' + x_var + ' and Log Tranform of ' + y_var)
             plt.xlabel(x_var)
             plt.ylabel('Log Transform of ' + y_var)
         else:
-            plt.scatter(x=df[x_var].dropna(), y=df[y_var].dropna())
+            plt.scatter(x=df[x_var].dropna(), y=df[y_var].dropna(), alpha=0.3)
             plt.title('Scatter Plot of ' + x_var + ' and ' + y_var)
             plt.xlabel(x_var)
             plt.ylabel(y_var)
     else:
         drops = df[[x_var,y_var]].dropna()
         if log == True:
-            plt.scatter(x=drops[x_var].dropna(), y=drops[y_var].dropna().apply(np.log))
+            plt.scatter(x=drops[x_var].dropna(), y=drops[y_var].dropna().apply(np.log), alpha=0.3)
             plt.title('Scatter Plot of ' + x_var + ' and Log Tranform of ' + y_var)
             plt.xlabel(x_var)
             plt.ylabel('Log Transform of ' + y_var)
         else:
-            plt.scatter(x=drops[x_var].dropna(), y=drops[y_var].dropna())
+            plt.scatter(x=drops[x_var].dropna(), y=drops[y_var].dropna(), alpha=0.3)
             plt.title('Scatter Plot of ' + x_var + ' and ' + y_var)
             plt.xlabel(x_var)
             plt.ylabel(y_var)
+    plt.show()
+    
+def heatmap(df):
+    '''
+    Function takes in a dataframe and creates a heatmap for all of the numerical features in the dataframe
+    '''
+    corr = df.corr()
+
+    mask = np.zeros_like(corr, dtype=np.bool)
+    mask[np.triu_indices_from(mask)] = True
+
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(25, 12))
+
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+    # Draw the heatmap with the mask and correct aspect ratio
+    sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0, linewidths=.5, cbar_kws={"shrink": .5}, annot=True)
+    plt.show()
+    
+def cat_barplot(df, x_col, y_col, log=False):
+    plt.figure(figsize=(17,9))
+    if log == True:
+        sns.barplot(y=df[x_col], x=df[y_col].dropna().apply(np.log),orient='h',ci=None,palette='Blues_d')
+        plt.title('Bar Plot of ' + x_col + ' and Log Tranform of ' + y_col)
+        plt.xlabel()
+        plt.ylabel('Log Transform of ' + y_col)
+    else:
+        sns.barplot(y=df[x_col].dropna(), x=df[y_col].dropna(),orient='h',ci=None,palette='Blues_d')
+        plt.title('Bar Plot of ' + x_col + ' and ' + y_col)
+        plt.xlabel(x_col)
+        plt.ylabel(y_col)
+    plt.show()
+
+def cat_boxplot(df, x_col, y_col, log=False):
+    plt.figure(figsize=(17,9))
+    if log == True:
+        sns.boxplot(x=df[x_col], y=df[y_col].dropna().apply(np.log),orient='v')
+        plt.title('Bar Plot of ' + x_col + ' and Log Tranform of ' + y_col)
+        plt.xlabel(x_col)
+        plt.ylabel('Log Transform of ' + y_col)
+    else:
+        sns.boxplot(x=df[x_col].dropna(), y=df[y_col].dropna(),orient='v')
+        plt.title('Bar Plot of ' + x_col + ' and ' + y_col)
+        plt.xlabel(x_col)
+        plt.ylabel(y_col)
     plt.show()
